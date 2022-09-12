12a7082b847857d45dd79d1a01958c62f5538df1
Christopher Stevers <christopher.stevers@protonmail.com>
2022-09-12 10:25:00 -0400

    prettier autofix and linting

--- src/components/list-section/index.js
+++ src/components/list-section/index.js
@@ -52,7 +52,7 @@ const ListSection = () => (
           </div>
           <div className={tw(`w-full lg:w-1/2 px-8`)}>
             <div className={tw(`lg:mb-12 lg:mb-0 pb-12 lg:pb-0 mt-16 lg:mt-0 mx-6 lg:mx-0`)}>
-              <FeatureSvg width="100%" height="100%" />
+              <FeatureSvg width='100%' height='100%' />
             </div>
           </div>
         </div>
