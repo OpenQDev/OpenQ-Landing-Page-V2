31d28c72b7aaf2d40c7959d7734e5547241b1412
Christopher Stevers <christopher.stevers@protonmail.com>
2022-09-27 15:14:11 -0400

    moving hotjar script around

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,6 @@
 import '@/styles/global.css';
 import '@fontsource/inter';
 import Script from 'next/script';
-import Head from 'next/head';
 
 import { setup } from 'twind';
 import { useEffect } from 'react';
@@ -27,7 +26,6 @@ export default function MyApp({ Component, pageProps }) {
     };
   }, [router.events]);
 
-
   return (
     <>
       <Script
@@ -45,23 +43,19 @@ export default function MyApp({ Component, pageProps }) {
         `}
       </Script>
 
-      <Head>
-      
-<script>function hotjar(){
-(function(h,o,t,j,a,r){
+      <Script
+        onLoad={()=>{
+           (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:3177116,hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')
-
-
-}
-    hotjar();
-</script></Head>
-      
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+  
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
