6217febb7ab519351ec165aac6e66a45e1ca3c31
Christopher Stevers <christopher.stevers@protonmail.com>
2022-09-27 14:41:47 -0400

    added hotjar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -42,6 +42,22 @@ export default function MyApp({ Component, pageProps }) {
         gtag('config', '${process.env.GOOGLE_ANALYTICS_ID}');
         `}
       </Script>
+
+      
+      <Script
+        strategy='afterInteractive'
+        dangerouslySETInnerHTML={{
+          __html: ` (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3177116,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+  `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
