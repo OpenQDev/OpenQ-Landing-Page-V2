efa004fd638f34ae3dac86e0d3dc5a3bf49a1fc8
Christopher Stevers <christopher.stevers@protonmail.com>
2022-09-30 11:51:45 -0400

    fixed sorting of created when bounties are complete

--- src/pages/contests.js
+++ src/pages/contests.js
@@ -26,6 +26,7 @@ export default function Home() {
       description: 'FlacoJones merged 20 commits in from Johnny-V:master',
       scrollY,
       fade: '',
+      price: '500'
     },
     {
       url: 'https://github.com/snapshot-labs/stamp/pull/12',
@@ -33,6 +34,7 @@ export default function Home() {
       description: 'bonustrack merged 7 commits into snapshot-labs:master from FlacoJones:master',
       scrollY,
       fade: '',
+      price: '300'
     },
     {
       url: 'https://github.com/snapshot-labs/stamp/pull/20',
@@ -40,6 +42,7 @@ export default function Home() {
       description: 'bonustrack merged 5 commits into master from sekhmet/jazzicon',
       scrollY,
       fade: '',
+      price: '300'
     },
     {
       url: 'https://github.com/snapshot-labs/stamp/pull/18',
@@ -47,7 +50,64 @@ export default function Home() {
       description: 'bonustrack merged 3 commits into master from sekhmet/selfid',
       scrollY,
       fade: '',
+      price: '300'
     },
+    {
+      url: 'https://github.com/honey-labs/honey-frontend/pull/162',
+      title: 'Add TPS banner #162',
+      description: 'BowTiedHeron merged 2 commits into honey-labs:development from eborrallo:development',
+      scrollY,
+      fade: '',
+      price: '200'
+    },
+    {
+      url: 'https://github.com/OpenQDev/OpenQ-Frontend/pull/376',
+      title: 'wallet connect buttons centered #376',
+      description: 'FlacoJones merged 1 commit into OpenQDev:development from ArcAnya:development',
+      scrollY,
+      fade: '',
+      price: '20'
+    },
+    {
+      url: 'https://github.com/OpenQDev/OpenQ-Frontend/pull/54',
+      title: 'Adds Jazzicon. Close #19 #54',
+      description: 'FlacoJones merged 1 commit into OpenQDev:development from ArcAnya:development',
+      scrollY,
+      fade: '',
+      price: '90'
+    },
+    {
+      url: 'https://github.com/OpenQDev/OpenQ-Frontend/pull/67',
+      title: 'First visit #67',
+      description: 'FlacoJones merged 2 commits into OpenQDev:development from Christopher-Stevers:first-visit on Feb 28',
+      scrollY,
+      fade: '',
+      price: '90'
+    },
+    {
+      url: 'https://github.com/OpenQDev/OpenQ-Frontend/pull/691',
+      title: 'Fixed claim text and pagination. #691',
+      description: 'FlacoJones merged 6 commits into OpenQDev:staging from Christopher-Stevers:WIP-categories',
+      scrollY,
+      fade: '',
+      price: '250'
+    },
+    {
+      url: 'https://github.com/OpenQDev/OpenQ-Frontend/pull/592',
+      title: 'Contest PayoutSchedule, Contract Wizard #592',
+      description: 'Christopher-Stevers merged 23 commits into OpenQDev:staging from ArcAnya:tierVolume',
+      scrollY,
+      fade: '',
+      price: '250'
+    },
+    {
+      url: 'https://github.com/OpenQDev/OpenQ-Frontend/pull/779',
+      title: 'Added emojis to TVL and Current Target Budget closes #774 #779 ',
+      description: 'FlacoJones merged 3 commits into OpenQDev:staging from Vinitvh:production',
+      scrollY,
+      fade: '',
+      price: '10'
+    }
   ];
   const cardSectionData = [
     { title: 'Release Payment automatically after merging PR', SVG: Add, body: 'Lorme ipsum' },
